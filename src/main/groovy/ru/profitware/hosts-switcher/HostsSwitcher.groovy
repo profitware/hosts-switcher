12ca75ca6ac187b315a03cf2348f6a31bc409751
bug2bug <S.Sobko@profitware.ru>
2016-05-16 15:48:46 +0300

    hostname flow

--- src/main/groovy/ru/profitware/hosts-switcher/HostsSwitcher.groovy
+++ src/main/groovy/ru/profitware/hosts-switcher/HostsSwitcher.groovy
@@ -19,9 +19,18 @@ class HostsSwitcher {
             case 1: hosts_content = hosts_content.readLines().findAll {
                         !it.contains(args[0])
                     }.join(sep); break;
-            default: hosts_content = hosts_content.contains(args[0])
-                   ? hosts_content.readLines().collect { 
-                         it.contains(args[0]) ? args.join(' ') : it 
+            default: hosts_content = args.inject(false) {
+                        acc, arg ->
+                        acc |= hosts_content.contains(arg);
+                        return acc;
+                     }
+                   ? hosts_content.readLines().collect {
+                         host_line ->
+                         return args.inject(false) {
+                             acc, arg ->
+                             acc |= host_line.contains(arg);
+                             return acc;
+                         } ? args.join(' ') : host_line;
                      }.join(sep)
                    : "$hosts_content$sep${ args.join(' ') }$sep";
         }
